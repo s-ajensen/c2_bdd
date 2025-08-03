9b9667311709e1d599f5534fee608f52156e944a
Alex Jensen <alex@endure.io>
2025-08-03 12:02:33 -0400

    cleans up characteristics

--- c2_bdd.hpp
+++ c2_bdd.hpp
@@ -1,12 +1,15 @@
 #ifndef BDD_HPP
 #define BDD_HPP
 
-#define describe(name, fn) TEST_CASE(name) { fn(); }
-#define context(name, fn) SECTION(name) { fn(); }
-#define it(name, fn) SECTION(name) { fn(); }
+#include <functional>
 
-#define before_each(fn) auto __setup = [&]() { fn(); }; __setup();
-#define after_each(fn) auto __cleanup = [&]() { fn(); }; \
+#define describe(name) TEST_CASE(name)
+#define context(name) SECTION(name)
+#define it(name) SECTION(name)
+
+#define before_each(...) auto __setup = [&]() { __VA_ARGS__ }; __setup();
+#define after_each(...) \
+auto __cleanup = [&]() { __VA_ARGS__ }; \
 struct __Cleanup { \
 std::function<void()> f; \
 ~__Cleanup() { f(); } \
@@ -18,7 +21,5 @@ std::function<void()> f; \
 #define should_ne(actual, expected) REQUIRE((actual) != (expected))
 #define should_throw(expr) REQUIRE_THROWS(expr)
 #define should_nothrow(expr) REQUIRE_NOTHROW(expr)
-
-#include <functional>
 
 #endif
